c68d825d0fe1358786d006065b05978ecbca50c7
Nataly Alvarado Lujan <nataly.al.luj@gmail.com>
2024-09-27 14:51:14 +0200

    Resuelto 👌

--- src/main/java/com/campusdual/classroom/Exercise11.java
+++ src/main/java/com/campusdual/classroom/Exercise11.java
@@ -6,6 +6,27 @@ public class Exercise11 {
         TVRemote blackRemote = new TVRemote("Negro");
         System.out.println(redRemote.getColor());
         System.out.println(blackRemote.getColor());
+
+        redRemote.turnOn();
+        redRemote.channelUp();
+        redRemote.channelDown();
+        redRemote.channelDown();
+        redRemote.channelDown();
+
+        System.out.println(redRemote.channel);
+
+        redRemote.volumeUp();
+        redRemote.volumeDown();
+        redRemote.volume = 0;
+
+        System.out.println(redRemote.volume);
+        redRemote.volumeDown();
+        redRemote.volumeDown();
+
+        System.out.println(redRemote.getColor());
+
+        redRemote.turnOff();
+
     }
 
 }
